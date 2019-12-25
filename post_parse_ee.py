4faa4bf9138e596c1cf256e4a458b4a33837e8c4
sophiakene <44840201+sophiakene@users.noreply.github.com>
2019-12-25 18:58:27 +0100

    Add files via upload
    Initialisation of Estonian text simplification process

---
+++ post_parse_ee.py
@@ -0,0 +1,59 @@
+ # -*- coding: UTF-8 -*-
+from collections import OrderedDict
+import subprocess
+import sys
+import random
+
+class PostParser():
+
+	"""def __init__(self, rfile, wfile):
+		self.rfile = rfile
+		self.wfile = wfile"""
+
+	def reform(self, rfile, wfile):
+		"""Aligns word and its corresponding analysis."""
+		output = ''
+		for line in rfile:
+			if line.startswith('"<'):
+				output += '\n'
+				#output += line.strip()+"\t"
+			output += line.strip()+"\t"
+		wfile.write(output)
+		return output
+
+	def makedic(self, rfile):
+		"""Creates a list of dictionaries for each sentence with words as keys and their corresponding analysis as values."""
+		sentences = []
+
+		rfile.seek(0)
+		for line in rfile:
+			if '"<s>"' in line:
+				pos = {}
+			if len(line.strip().split("\t")) == 2: #not line.startswith('"<s>"') and 
+				key = (line.strip('"').split("\t"))[0]
+				value = (line.strip('"').split("\t"))[1]
+				if key.strip('"<').strip('>"') in pos:
+					key = key.strip('"<').strip('>"') + random.randint(0,10)*' '
+				pos[key.strip('"<"').strip('">"')] = value
+			if '"</s>"' in line:
+				sentences.append(pos)
+		return sentences
+
+
+def main():
+
+	#file = sys.argv[0] #TODO relative file done in pipeline.
+
+	subprocess.call(['./parser17.sh', '../parsethis.txt'], cwd='EstCG-parser')
+	#creates neu.txt.cg3 in the same directory
+
+	with open('parsethis.txt.cg3') as parsed_doc, open('parsethis.txt', 'w+', encoding='utf-8') as writefile:
+		lines = parsed_doc.readlines()
+		postparser = PostParser()
+		postprocessed = postparser.reform(lines, writefile)
+		sentences = postparser.makedic(writefile)
+		print(sentences)
+
+if __name__ == '__main__':
+	main()
+
